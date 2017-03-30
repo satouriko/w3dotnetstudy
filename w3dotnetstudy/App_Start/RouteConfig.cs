e7573bef8479506e7f71055dcb5a3538570d3b41
LI JIAHAO <lijiahao99131@gmail.com>
2017-03-30 15:53:38 +0800

    添加了一些路由和控制器

--- w3dotnetstudy/App_Start/RouteConfig.cs
+++ w3dotnetstudy/App_Start/RouteConfig.cs
@@ -14,10 +14,17 @@ namespace w3dotnetstudy
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "NewInteresting",
+                url: "New/Interesting/{name}",
+                defaults: new { controller = "New", action = "ShowName" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
---
+++ w3dotnetstudy/Controllers/NewController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace w3dotnetstudy.Controllers
+{
+    public class NewController : Controller
+    {
+        // GET: New
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: New
+        public ActionResult ShowName(string name)
+        {
+            return View();
+        }
+    }
+}
